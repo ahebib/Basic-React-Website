12b3c8089dc1e5a4371e465e9f3ae923193eeea7
Ammar Hebib <ammar.hebib@gmail.com>
2021-04-08 22:14:54 -0400

    Initial React Template
    Removed unwanted styles and code snippets

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="home-title">React Template App</h1>
+
+      <div className="to-do">
+        <h2>To-Do:</h2>
+        <ul>
+          <li>Test connections between public files and src</li>
+          <li>Figure out routing</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
