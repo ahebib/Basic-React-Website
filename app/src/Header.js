be0eacf41297be8e8a16761d2fa4f27474507f0d
Ammar Hebib <ammar.hebib@gmail.com>
2021-04-10 20:39:22 -0400

    Add template header

---
+++ app/src/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="site-header">
+    	Templates
+    </div>
+  );
+}
+
+export default Header;
--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './Header';
 import Footer from './Footer';
 import reportWebVitals from './reportWebVitals';
 import './Index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+  	<Header />
     <App />
     <Footer />
   </React.StrictMode>,
