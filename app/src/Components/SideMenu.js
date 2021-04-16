125de379504572904402a7863c3e0faf9737d05d
Ammar Hebib <ammar.hebib@gmail.com>
2021-04-15 23:21:35 -0400

    Make side menu collapse based off state.

--- app/src/Components/SideMenu.js
+++ app/src/Components/SideMenu.js
@@ -1,14 +1,33 @@
+import React from 'react';
 import './SideMenu.css';
 
-function SideMenu() {
-	return (
-		<div className="side-menu">
-			<ul className="sm-links">
-				<li><a href="/">Home</a></li>
-				<li><a href="/routing">Routing</a></li>
-			</ul>
-		</div>
-	)
+class SideMenu extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { hidden: 'true' }
+	}
+
+	toggleMenu() {
+		this.setState({ hidden: this.state.hidden === 'false' ? 'true' : 'false' });
+	}
+
+	render() {
+		return (
+			<div>
+				<button className="side-menu-button" onClick={this.toggleMenu.bind(this)}>Menu</button>
+				<div className={this.state.hidden === 'false' ? 'side-menu active-menu' : 'side-menu hidden-menu'}>
+					<div className="sm-content">
+					<ul className="sm-links">
+						<li><a href="/">Home</a></li>
+						<li><a href="/routing">Routing</a></li>
+						<li><div onClick={this.toggleMenu.bind(this)}>Close</div></li>
+					</ul>
+					</div>
+				</div>
+			</div>
+		);
+		
+	}
 }
 
 export default SideMenu;
